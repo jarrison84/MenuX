d3203eaf83f61e15bb1ce0e86f0fcc3b051a1473
jarrison84 <jarrisonquitero123@gamil.com>
2025-05-07 22:39:23 -0500

    Cambios base datosAgregar archivos de proyecto.

---
+++ MenuX/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MenuX.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MenuX.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }  
+        
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
