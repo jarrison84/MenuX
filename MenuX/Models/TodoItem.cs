8197cce02227a7a8fb4201ef7a9e0f2a230466a0
jarrison84 <jarrisonquitero123@gamil.com>
2025-05-08 00:22:25 -0500

    Revert "Cambios base datosAgregar archivos de proyecto."
    This reverts commit d3203eaf83f61e15bb1ce0e86f0fcc3b051a1473.

--- MenuX/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MenuX.Models
-{
-    public class TodoItem
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public Boolean? IsComplete { get; set; }    
-    }
-}
